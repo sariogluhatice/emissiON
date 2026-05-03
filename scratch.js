361ed7eee7d02b61e995c8004eeb0c362816d077
Sedef Esra Kazan <21SOFT1035@isik.edu.tr>
2026-05-03 12:09:51 +0300

    gereksiz test dosyaları kaldırıldı

--- scratch.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const key = process.env.GEMINI_API_KEY;
-const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=${key}`;
-fetch(url, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ contents: [{ parts: [{ text: "Merhaba" }] }] })
-}).then(res => res.json()).then(console.log).catch(console.error);
